72427c3dd5096b3b96896e6b15de043ce425b3db
cr0nuss <102534139+cr0nuss@users.noreply.github.com>
2022-04-28 20:18:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     float b = -7.9;
     double n;
     if (a >= b) n = pow(a - b, (0.33));
-    else if (a < b) n = (pow(a, 2) + ((a - b) / sin(a * b)));
+    else if (a < b) n = (pow(a, 2) + ((a - b) / (sin(a * b))));
     if (n < b) {
         cout << ((n + a) / (-b) + sqrt(pow(sin(a), 2) - cos(n)));
     } else if (n == b) cout << (tan(n * a) + pow(b, 2));
